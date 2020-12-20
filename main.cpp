8ec06d0f4bcbc32456d829817590bafb9e4d1809
7assan-5alloof <hassankhalloof20#gmail.com>
2020-12-20 09:33:34 +0400

    Write part of the program

--- main.cpp
+++ main.cpp
@@ -4,6 +4,41 @@ using namespace std;
 
 int main()
 {
-    cout << "Hello world!" << endl;
+    int dayNumber, numberOfDays;
+    cout << "Enter today as follows: 0 for Friday, 1 for Saturday, 2 for Sunday, 3 for Monday, 4 for Tuesday, 5 for Wednesday, and 6 for Thursday: ";
+    cin >> dayNumber;
+    string day;
+    if (dayNumber == 0)
+    {
+    day = "Friday";
+    }
+    else if (dayNumber == 1)
+    {
+    day = "Saturday";
+    }
+    else if (dayNumber == 2)
+    {
+    day = "Sunday";
+    }
+    else if (dayNumber == 3)
+    {
+    day = "Monday";
+    }
+    else if (dayNumber == 4)
+    {
+    day = "Tuesday";
+    }
+    else if (dayNumber == 5)
+    {
+    day = "Wednesday";
+    }
+    else if (dayNumber == 6)
+    {
+    day = "Thursday";
+    }
+    cout << "Enter the number of days from today: ";
+    cin >> numberOfDays;
+
+
     return 0;
 }
